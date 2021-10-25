4f7b05d934dd22c1f8e91637978e6fb2bdf8042a
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-10-25 20:42:18 +0200

    add: fragments

---
+++ app/src/main/java/com/app/bewise/ui/library/fragments/ThirdFragment.java
@@ -0,0 +1,23 @@
+package com.app.bewise.ui.library.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.app.bewise.R;
+
+
+public class ThirdFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_third, container, false);
+    }
+}
