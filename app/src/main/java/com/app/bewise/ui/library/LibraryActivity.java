6cfc6a41772ccb4cdd1a17a5b3030325667af50a
agostinhodossantos <agostinhodossantos27@gmail.com>
2021-10-27 05:02:24 -0700

    add card style

--- app/src/main/java/com/app/bewise/ui/library/LibraryActivity.java
+++ app/src/main/java/com/app/bewise/ui/library/LibraryActivity.java
@@ -15,25 +15,16 @@ import com.google.android.material.tabs.TabLayout;
 
 public class LibraryActivity extends AppCompatActivity {
 
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
 
         initUI();
-        tabLayout.setupWithViewPager(viewPager);
-        VPadapter vPadapter = new VPadapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
-        vPadapter.addFragment(new FirstFragment(), "Chat");
-        vPadapter.addFragment(new SecondFragment(), "Status");
-        vPadapter.addFragment(new ThirdFragment(), "Call");
-        viewPager.setAdapter(vPadapter);
+    }
+    private void initUI() {
+
     }
 
-    private void initUI() {
-        viewPager = findViewById(R.id.vp_library);
-        tabLayout = findViewById(R.id.tl_library);
-    }
+
 }
