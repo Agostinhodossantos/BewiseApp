ff552759ab2b4144dca81e2c96768472f0ae9115
agostinhodossantos <agostinhodossantos27@gmail.com>
2021-10-28 06:04:58 -0700

    add vertical_list

--- app/src/main/java/com/app/bewise/ui/library/LibraryActivity.java
+++ app/src/main/java/com/app/bewise/ui/library/LibraryActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 import com.app.bewise.R;
 import com.app.bewise.adapters.BooksCategoryAdapter;
 import com.app.bewise.adapters.BooksListAdapter;
+import com.app.bewise.adapters.VerticalBooksListAdapter;
 import com.app.bewise.model.Book;
 import com.app.bewise.model.BookCategory;
 
@@ -19,7 +20,7 @@ import java.util.List;
 
 public class LibraryActivity extends AppCompatActivity {
 
-    RecyclerView rv_books, rv_books_category;
+    RecyclerView rv_books, rv_books_category, rv_my_books;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +29,7 @@ public class LibraryActivity extends AppCompatActivity {
         initUI();
         getBooks();
         getCategory();
+        getMyBook();
     }
 
     private void getBooks() {
@@ -60,9 +62,23 @@ public class LibraryActivity extends AppCompatActivity {
         rv_books_category.setAdapter(adapter);
     }
 
+    private void getMyBook() {
+        List<Book> bookList = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            Book book = new Book("", "", "", "", "");
+            bookList.add(book);
+        }
+
+        VerticalBooksListAdapter adapter = new VerticalBooksListAdapter(this, bookList);
+        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(1, LinearLayoutManager.VERTICAL);
+        rv_my_books.setLayoutManager(layoutManager);
+        rv_my_books.setAdapter(adapter);
+    }
+
     private void initUI() {
         rv_books = findViewById(R.id.rv_books);
         rv_books_category = findViewById(R.id.rv_books_category);
+        rv_my_books = findViewById(R.id.rv_books_vertical);
     }
 
 
