8b1077d9e1d8542a405522d5a760db374a3ebd9d
agostinhodossantos <agostinhodossantos27@gmail.com>
2021-10-29 04:40:46 -0700

    add book_list

---
+++ app/src/main/java/com/app/bewise/ui/library/BookListActivity.java
@@ -0,0 +1,16 @@
+package com.app.bewise.ui.library;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.bewise.R;
+
+public class BookListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_list);
+    }
+}
--- app/src/main/java/com/app/bewise/ui/main/MainActivity.java
+++ app/src/main/java/com/app/bewise/ui/main/MainActivity.java
@@ -1,4 +1,4 @@
-package com.app.bewise.ui;
+package com.app.bewise.ui.main;
 
 import android.content.res.ColorStateList;
 import android.graphics.Color;
