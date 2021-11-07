95a435b88ae75ed071d49d76335ba5e6a3a8751a
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-11-07 20:01:05 +0200

    create login page

--- app/src/main/java/com/app/bewise/ui/login/LoginActivity.java
+++ app/src/main/java/com/app/bewise/ui/login/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.app.bewise.ui.login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -8,9 +9,21 @@ import com.app.bewise.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initUI();
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    private void initUI() {
+        toolbar = findViewById(R.id.toolbar);
     }
 }
