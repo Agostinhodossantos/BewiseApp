76e11abb0777f3ba80d47237ac706f7802981911
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-11-07 20:18:59 +0200

    create register

--- app/src/main/java/com/app/bewise/ui/login/RegisterActivity.java
+++ app/src/main/java/com/app/bewise/ui/login/RegisterActivity.java
@@ -1,6 +1,7 @@
 package com.app.bewise.ui.login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -8,9 +9,20 @@ import com.app.bewise.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        initUI();
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    private void initUI() {
+        toolbar = findViewById(R.id.toolbar);
     }
 }
