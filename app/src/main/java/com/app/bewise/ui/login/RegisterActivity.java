5b6e94e2a4554c2998496f9c393b5992e104db7d
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-11-07 21:43:01 +0200

    merge changes

--- app/src/main/java/com/app/bewise/ui/login/RegisterActivity.java
+++ app/src/main/java/com/app/bewise/ui/login/RegisterActivity.java
@@ -3,13 +3,17 @@ package com.app.bewise.ui.login;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.app.bewise.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    private TextView tv_create_by_email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +23,20 @@ public class RegisterActivity extends AppCompatActivity {
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        tv_create_by_email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), EmailCreateAcountActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
     private void initUI() {
         toolbar = findViewById(R.id.toolbar);
+        tv_create_by_email = findViewById(R.id.tv_create_by_email);
     }
 }
