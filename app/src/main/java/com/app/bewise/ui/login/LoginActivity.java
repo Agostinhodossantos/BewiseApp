eb849a35069e84ac61a1ada833aeeb4cac4d9f0c
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-11-07 10:00:08 +0200

    add login

---
+++ app/src/main/java/com/app/bewise/ui/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.app.bewise.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.bewise.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
