e97c0c23aeac78b67435807f4c811f599034b140
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-11-07 18:38:42 +0200

    add RegisterActivity

--- app/src/main/java/com/app/bewise/ui/login/OnboardingActivity.java
+++ app/src/main/java/com/app/bewise/ui/login/OnboardingActivity.java
@@ -3,7 +3,10 @@ package com.app.bewise.ui.login;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.app.bewise.R;
 import com.app.bewise.adapters.OnboardingPagerAdapter;
@@ -18,6 +21,7 @@ public class OnboardingActivity extends AppCompatActivity {
 
     ViewPager viewPager;
     CircleIndicator circleIndicator;
+    TextView tv_login, tv_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +31,30 @@ public class OnboardingActivity extends AppCompatActivity {
          initUI();
          initAds();
 
+         tv_login.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                 startActivity(intent);
+             }
+         });
+
+         tv_register.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                 startActivity(intent);
+             }
+         });
+
 
     }
 
     private void initUI() {
         viewPager = findViewById(R.id.vp_onboarding);
         circleIndicator = findViewById(R.id.indicator);
+        tv_login = findViewById(R.id.tv_login);
+        tv_register = findViewById(R.id.tv_register);
     }
 
     private void initAds() {
@@ -43,7 +65,6 @@ public class OnboardingActivity extends AppCompatActivity {
 
     private List<Onboarding> getOnboarding() {
         List<Onboarding> onboardings = new ArrayList<>();
-        onboardings.add(new Onboarding("", "", 1));
         onboardings.add(new Onboarding("", "", 1));
         onboardings.add(new Onboarding("", "", 1));
         onboardings.add(new Onboarding("", "", 1));
