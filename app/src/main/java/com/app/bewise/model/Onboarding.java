3b9ab4eb6b46b4fbca9553ede5877fab5038bd47
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-11-07 12:02:32 +0200

    create onboarding

---
+++ app/src/main/java/com/app/bewise/model/Onboarding.java
@@ -0,0 +1,38 @@
+package com.app.bewise.model;
+
+public class Onboarding {
+    private String title, description;
+    private int img;
+
+    public Onboarding(String title, String description, int img) {
+        this.title = title;
+        this.description = description;
+        this.img = img;
+    }
+
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+}
