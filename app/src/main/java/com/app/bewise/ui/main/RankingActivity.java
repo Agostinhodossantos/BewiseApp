6c49451d974d32db699ea0ae515d0f1d9ce67154
agostinhodossantos <agostinhodossantos27@gmail.com>
2021-11-08 04:43:39 -0800

    add ranking

--- app/src/main/java/com/app/bewise/ui/main/RankingActivity.java
+++ app/src/main/java/com/app/bewise/ui/main/RankingActivity.java
@@ -1,6 +1,7 @@
 package com.app.bewise.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
@@ -16,14 +17,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RankingActivity extends AppCompatActivity {
+
     private RecyclerView rv_ranking;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
         initUI();
         initRanking();
+
+        toolbar.setTitle("Top users");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+
 
     private void initRanking() {
 
@@ -40,6 +49,7 @@ public class RankingActivity extends AppCompatActivity {
     }
 
     private void initUI() {
+        toolbar = findViewById(R.id.toolbar);
         rv_ranking = findViewById(R.id.rv_ranking);
     }
 }
