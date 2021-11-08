e2100e8b2863c198e44aa57d21cd28854f8b8163
agostinhodossantos <agostinhodossantos27@gmail.com>
2021-11-08 01:40:58 -0800

    create card_profile

--- app/src/main/java/com/app/bewise/ui/login/LoginActivity.java
+++ app/src/main/java/com/app/bewise/ui/login/LoginActivity.java
@@ -3,13 +3,18 @@ package com.app.bewise.ui.login;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.app.bewise.R;
+import com.app.bewise.ui.UserProfileActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Toolbar toolbar;
+    private Toolbar toolbar;
+    private TextView tv_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +25,20 @@ public class LoginActivity extends AppCompatActivity {
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        tv_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, UserProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
     private void initUI() {
+        tv_register = findViewById(R.id.tv_register);
         toolbar = findViewById(R.id.toolbar);
     }
 }
