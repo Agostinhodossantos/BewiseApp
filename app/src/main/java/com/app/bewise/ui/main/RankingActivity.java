52ed586301fb047496378cc5e48f093372dad2ab
agostinhodossantos <agostinhodossantos27@gmail.com>
2021-11-08 04:20:34 -0800

    update ranking

--- app/src/main/java/com/app/bewise/ui/main/RankingActivity.java
+++ app/src/main/java/com/app/bewise/ui/main/RankingActivity.java
@@ -1,11 +1,19 @@
 package com.app.bewise.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
 
 import com.app.bewise.R;
+import com.app.bewise.adapters.JobsListAdapter;
+import com.app.bewise.adapters.RankingAdapter;
+import com.app.bewise.model.Ranking;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RankingActivity extends AppCompatActivity {
     private RecyclerView rv_ranking;
@@ -14,6 +22,21 @@ public class RankingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
         initUI();
+        initRanking();
+    }
+
+    private void initRanking() {
+
+        List<Ranking> rankingList = new ArrayList<>();
+        rankingList.add(new Ranking(1, 3, ""));
+        rankingList.add(new Ranking(1, 3, ""));
+        rankingList.add(new Ranking(1, 3, ""));
+        rankingList.add(new Ranking(1, 3, ""));
+
+        RankingAdapter adapter = new RankingAdapter(this, rankingList);
+        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(1, LinearLayoutManager.VERTICAL);
+        rv_ranking.setLayoutManager(layoutManager);
+        rv_ranking.setAdapter(adapter);
     }
 
     private void initUI() {
