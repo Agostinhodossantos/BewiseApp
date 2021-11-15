d290099d3b6a73781504a1380335370d9170660c
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-11-15 21:15:33 +0200

    add QuizGameActivity

---
+++ app/src/main/java/com/app/bewise/ui/study/game/QuizGameActivity.java
@@ -0,0 +1,16 @@
+package com.app.bewise.ui.study.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.bewise.R;
+
+public class QuizGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz_game);
+    }
+}
