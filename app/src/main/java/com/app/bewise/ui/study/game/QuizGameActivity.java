a32de9b5ba73f785370d3240e872ec73a43e19bb
agostinhodossantos <agostinhodossantos27@gmail.com>
2021-11-16 05:22:00 -0800

    update card question

--- app/src/main/java/com/app/bewise/ui/study/game/QuizGameActivity.java
+++ app/src/main/java/com/app/bewise/ui/study/game/QuizGameActivity.java
@@ -3,6 +3,9 @@ package com.app.bewise.ui.study.game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.app.bewise.R;
 
@@ -12,5 +15,11 @@ public class QuizGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_game);
+
+        Window window = this.getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(getColor(R.color.purple_700));
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE);
     }
 }
