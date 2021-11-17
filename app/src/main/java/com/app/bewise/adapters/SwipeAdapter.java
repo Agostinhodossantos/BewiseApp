637ff2f78d8dd0dabe8dbb548d4b4c2767edbd57
agostinhodossantos <agostinhodossantos27@gmail.com>
2021-11-17 03:06:51 -0800

    add SwipeAdapter

---
+++ app/src/main/java/com/app/bewise/adapters/SwipeAdapter.java
@@ -0,0 +1,86 @@
+package com.app.bewise.adapters;
+
+import android.media.MediaPlayer;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.app.bewise.R;
+
+import java.util.List;
+
+public class SwipeAdapter extends RecyclerView.Adapter<VideoAdapter.VideoViewHolder> {
+
+    private List<VideoItem> mVideoItem;
+
+    public SwipeAdapter(List<VideoItem> mVideoItem) {
+        this.mVideoItem = mVideoItem;
+    }
+
+    @NonNull
+    @Override
+    public VideoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new VideoViewHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_video_container, parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VideoViewHolder holder, int position) {
+        holder.setVideoData(mVideoItem.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mVideoItem.size();
+    }
+
+    static class VideoViewHolder extends RecyclerView.ViewHolder {
+
+        VideoView mVideoView;
+        TextView txtTitle, txtDesc;
+        ProgressBar progressBar;
+
+        public VideoViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mVideoView = itemView.findViewById(R.id.videoview);
+            txtDesc = itemView.findViewById(R.id.txtDescription);
+            txtTitle = itemView.findViewById(R.id.txtTitle);
+            progressBar = itemView.findViewById(R.id.progressBar);
+        }
+        void setVideoData(VideoItem video) {
+            txtTitle.setText(video.videoTitle);
+            txtDesc.setText(video.videoDesc);
+            mVideoView.setVideoPath(video.videoUrl);
+            mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    progressBar.setVisibility(View.GONE);
+                    mp.start();
+
+                    float videoRatio = mp.getVideoWidth() / (float) mp.getVideoHeight();
+                    float screenRatio = mVideoView.getWidth() / (float) mVideoView.getHeight();
+                    float scale = videoRatio / screenRatio;
+
+                    if (scale >= 1f) {
+                        mVideoView.setScaleX(scale);
+                    } else {
+                        mVideoView.setScaleY(1f / scale);
+                    }
+                }
+            });
+
+            mVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mp.start();
+                }
+            });
+        }
+    }
+}
