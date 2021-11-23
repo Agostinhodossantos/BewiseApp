b399f8349bb86e829b7fac411c980b1d0a4c3ca4
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-11-23 13:29:11 +0200

    add provider

---
+++ app/src/main/java/com/app/bewise/provider/UserProvider.java
@@ -0,0 +1,13 @@
+package com.app.bewise.provider;
+
+import com.app.bewise.model.UserBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class UserProvider {
+    public List<UserBuilder> getUsers() {
+        List<UserBuilder> usersList = new ArrayList<>();
+        return usersList;
+    }
+}
