fb57f3d9a5e76b585174aeb099b4d956afaf59fa
Agostinhodossantos <agostinhodossantos27@gmail.com>
2021-12-01 09:12:26 +0200

    update Book class: add category params

--- app/src/main/java/com/app/bewise/model/Book.java
+++ app/src/main/java/com/app/bewise/model/Book.java
@@ -1,17 +1,18 @@
 package com.app.bewise.model;
 
 public class Book {
-    private String id, title, author, img, url;
+    private String id, title,category ,author, img, url;
 
-    public Book(String id, String title, String author, String img, String url) {
+    public Book() {
+    }
+
+    public Book(String id, String title, String category, String author, String img, String url) {
         this.id = id;
         this.title = title;
+        this.category = category;
         this.author = author;
         this.img = img;
         this.url = url;
-    }
-
-    public Book() {
     }
 
     public String getId() {
@@ -28,6 +29,14 @@ public class Book {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
     }
 
     public String getAuthor() {
